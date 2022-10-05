43e54fcdeae724a3a0e5370d38552a59fce04510
K-ash-ish <kashishsondhiya@gmail.com>
2022-10-05 18:00:25 +0530

    local database created with dynamic routes matched with ID's

--- app.js
+++ app.js
@@ -4,20 +4,26 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const lodash = require("lodash");
 const ejs = require("ejs");
-
+const mongoose = require('mongoose');
+const uri = "mongodb://localhost:27017/blogAppDB";
+mongoose.connect(uri);
+const blogSchema = new mongoose.Schema({
+  postTitle: String,
+  postBody: String
+});
+const Blog = mongoose.model("Blog", blogSchema);
+const blog = new Blog({
+  postTitle:"Day 1",
+  postBody:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod te mpor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+});
+// blog.save(); // one entry is saved
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const posts = [
-  {
-    postTitle: "Day 1",
-    postBody:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod te mpor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  },
-];
 const app = express();
 
 app.set("view engine", "ejs");
@@ -26,16 +32,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("home", { homeContent: homeStartingContent, allPosts: posts });
+  Blog.find({}, function(err, blogs){
+    res.render("home", { homeContent: homeStartingContent, allPosts: blogs });
+  })
 });
-app.get("/posts/:post", function (req, res) {
-  let postRoute = lodash.lowerCase(req.params.post);
-  posts.forEach((element) => {
-    if (lodash.lowerCase(element.postTitle) === postRoute) {
+app.get("/posts/:postId", function (req, res) {
+  let postRoute = (req.params.postId);
+  console.log(postRoute);
+  Blog.findById({_id: postRoute}, function(err, blogs){
+    if(err){
+      console.log("Not Found");
       res.render("post", {
-        postTitle: element.postTitle,
-        postBody: element.postBody,
+        postTitle: "Not Found",
+        postBody: "",
       });
+    }
+    else{
+      res.render("post", {
+        postTitle: blogs.postTitle,
+        postBody: blogs.postBody,
+      });
+
     }
   });
 });
@@ -51,12 +68,15 @@ app.get("/compose", function (req, res) {
 });
 
 app.post("/compose", function (req, res) {
-  const post = {
+  const newBlog = new Blog( {
     postTitle: req.body.postTitle,
     postBody: req.body.post,
-  };
-  posts.push(post);
-  res.redirect("/");
+  });
+  newBlog.save(function(err){
+    if(!err){
+      res.redirect("/");
+    }
+  });
 });
 
 app.listen(3000, function () {
